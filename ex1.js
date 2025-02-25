dd031b41210a90ca7cb2c4354ed3dd874d1a0586
VictorGB0fc <k.victor@escola.pr.gov.br>
2025-02-25 08:04:55 -0300

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,10 @@
+#javascript
+//contar caracteres em uma String
+function contarCaracteres(str) {
+let contagem = {};
+for (let char of str) {
+    contagem[char] = (contagem[char] || 0) + 1;
+}
+console.log(contagem);
+}
+contarCaracteres("Banana")
